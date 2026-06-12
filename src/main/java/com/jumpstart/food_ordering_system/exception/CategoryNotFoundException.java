27e7b0e4977e0e6794fbbfc2ad77236feca302f7
koena08 <kayletsie@gmail.com>
2026-06-12 08:39:27 +0200

    Chore: Packages configured

--- src/main/java/com/jumpstart/food_ordering_system/exception/CategoryNotFoundException.java
+++ src/main/java/com/jumpstart/food_ordering_system/exception/CategoryNotFoundException.java
@@ -4,6 +4,7 @@ package com.jumpstart.food_ordering_system.exception;
 public class CategoryNotFoundException extends RuntimeException {
 
     public CategoryNotFoundException(String message) {
+        
         super(message);
     }
 }

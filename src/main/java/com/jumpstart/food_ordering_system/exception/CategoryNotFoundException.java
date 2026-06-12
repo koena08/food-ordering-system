c6754c5e86d20bf770194aa4ca5b6aa2b74aed76
koena08 <kayletsie@gmail.com>
2026-06-12 08:46:41 +0200

    Fix: Indentation

--- src/main/java/com/jumpstart/food_ordering_system/exception/CategoryNotFoundException.java
+++ src/main/java/com/jumpstart/food_ordering_system/exception/CategoryNotFoundException.java
@@ -4,7 +4,7 @@ package com.jumpstart.food_ordering_system.exception;
 public class CategoryNotFoundException extends RuntimeException {
 
     public CategoryNotFoundException(String message) {
-        
+
         super(message);
     }
 }

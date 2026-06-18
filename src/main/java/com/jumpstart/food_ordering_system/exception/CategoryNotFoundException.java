c03f30e6fe9d90e8e9392d399381ebd7619d5c23
koena08 <kayletsie@gmail.com>
2026-06-18 17:25:18 +0200

    feat(category): Response status updated

--- src/main/java/com/jumpstart/food_ordering_system/exception/CategoryNotFoundException.java
+++ src/main/java/com/jumpstart/food_ordering_system/exception/CategoryNotFoundException.java
@@ -1,6 +1,10 @@
 package com.jumpstart.food_ordering_system.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 //Custom exceptions provide specific error definitions for the application to handle unique business logic failures
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CategoryNotFoundException extends RuntimeException {
 
     public CategoryNotFoundException(String message) {

1c986d521776700b0fd3feb8a1eb249e82875d68
koena08 <kayletsie@gmail.com>
2026-06-22 19:43:32 +0200

    feat(menu): controller (create + read)

--- src/main/java/com/jumpstart/food_ordering_system/controller/MenuController.java
+++ src/main/java/com/jumpstart/food_ordering_system/controller/MenuController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/menu")
+@RequestMapping("/api/menu")
 @RequiredArgsConstructor
 public class MenuController {
 

15dfff3c288e9dea9a8eee40c8f15653eb3b7bf2
koena08 <kayletsie@gmail.com>
2026-06-23 11:44:30 +0200

    feat(menu): filtering & pagination business logic

--- src/main/java/com/jumpstart/food_ordering_system/service/MenuService.java
+++ src/main/java/com/jumpstart/food_ordering_system/service/MenuService.java
@@ -2,11 +2,17 @@ package com.jumpstart.food_ordering_system.service;
 
 import com.jumpstart.food_ordering_system.dto.MenuDTO;
 import com.jumpstart.food_ordering_system.response.Response;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
 
 public interface MenuService {
     Response<MenuDTO> createMenu(MenuDTO dto);
-    Response<List<MenuDTO>> getAllMenus();
+
+    //Upgrade: handles pagination & filtering
+    Response<Page<MenuDTO>> getAllMenus(Long categoryId, String search, int page, int size, String sort);
+
     Response<MenuDTO> getMenuById(Long id);
+
+    //New CRUD ops
+    Response<MenuDTO> updateMenu(Long id, MenuDTO dto);
+    Response<Void> deleteMenu(Long id);
 }
